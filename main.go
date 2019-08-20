928a470a71419a353d9174cab7991b69ec0f5ad9
PawelPleskaczynski <pawelpleskaczynski@gmail.com>
2019-08-20 13:41:25 +0200

    added support for files from Cassini mission

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func check(e error) {
@@ -26,14 +27,16 @@ func getDigits(input string, regex *regexp.Regexp) (int, error) {
 	return height, err
 }
 
-func getDimensions(label *os.File) (int, int, int) {
+func getDimensions(label *os.File) (int, int, int, string) {
 	var width, height, bitdepth int
+	var missionName string
 	var err error
 
 	// endRegexWhitespace := regexp.MustCompile(`\s{50,}`)
 	linesRegex := regexp.MustCompile(`(?:LINES)(?:\s+)?(?:=)(?:\s+)?(\d+)`)
 	lineSamplesRegex := regexp.MustCompile(`(?:LINE_SAMPLES)(?:\s+)?(?:=)(?:\s+)?(\d+)`)
 	sampleBitsRegex := regexp.MustCompile(`(?:BITS)(?:\s+)?(?:=)(?:\s+)?(\d+)`)
+	missionNameRegex := regexp.MustCompile(`(?:MISSION_NAME)(?:\s+)?(?:=)(?:\s+)(?:")?(.+)(?:")`)
 
 	scanner := bufio.NewScanner(label)
 	buf := make([]byte, 0, 64*1024*1024)
@@ -66,6 +69,11 @@ func getDimensions(label *os.File) (int, int, int) {
 			}
 		}
 
+		if missionNameRegex.MatchString(scanner.Text()) {
+			missionNameStr := scanner.Text()
+			missionName = missionNameRegex.FindStringSubmatch(missionNameStr)[1]
+		}
+
 		// if endRegexWhitespace.MatchString(scanner.Text()) {
 		// 	whitespaceLength := len(endRegexWhitespace.FindStringSubmatch(scanner.Text())[0])
 		// 	endbyte += whitespaceLength
@@ -86,7 +94,7 @@ func getDimensions(label *os.File) (int, int, int) {
 	// 	endbyte = 0
 	// }
 
-	return width, height, bitdepth
+	return width, height, bitdepth, missionName
 }
 
 func main() {
@@ -97,6 +105,8 @@ func main() {
 	length := len(*filename)
 
 	var width, height, bitdepth int
+	var missionName string
+
 	buf := bytes.NewBuffer(nil)
 	file, err := os.Open(*filename)
 	check(err)
@@ -107,19 +117,41 @@ func main() {
 	if *labelname != "" {
 		label, err := os.Open(*labelname)
 		check(err)
-		width, height, bitdepth = getDimensions(label)
+		width, height, bitdepth, missionName = getDimensions(label)
 	}
 
 	if height == 0 || width == 0 || bitdepth == 0 {
 		label, err := os.Open(*filename)
 		check(err)
-		width, height, bitdepth = getDimensions(label)
+		width, height, bitdepth, missionName = getDimensions(label)
 		if height == 0 || width == 0 || bitdepth == 0 {
 			log.Fatalln("Can't get all needed image attributes, exiting")
 		}
 	}
 
+	if missionName != "" {
+		fmt.Printf("Mission: %s\n", missionName)
+	}
 	fmt.Printf("Width: %d, height: %d, bit depth: %d\n", width, height, bitdepth)
+
+	if strings.EqualFold(missionName, "CASSINI-HUYGENS") {
+		fileslice = fileslice[4192:]
+		j, count512, count12 := 0, 0, 0
+		array512 := make([]byte, len(fileslice))
+		for i := 0; i < len(fileslice); i++ {
+			if count512 < 1024 {
+				array512[j] = fileslice[i]
+				j++
+				count512++
+			} else if count12 < 23 && count512 == 1024 {
+				count12++
+			} else if count12 == 23 {
+				count512 = 0
+				count12 = 0
+			}
+		}
+		fileslice = array512
+	}
 
 	if bitdepth == 8 {
 		image := image.NewGray(image.Rect(0, 0, width, height))
@@ -128,21 +160,7 @@ func main() {
 		check(err)
 		defer out.Close()
 		png.Encode(out, image)
-	} else if bitdepth == 12 {
-		image := image.NewGray16(image.Rect(0, 0, width, height))
-		image.Pix = fileslice
-		out, err := os.Create((*filename)[:length-4] + ".png")
-		check(err)
-		defer out.Close()
-		png.Encode(out, image)
-	} else if bitdepth == 16 {
-		image := image.NewGray16(image.Rect(0, 0, width, height))
-		image.Pix = fileslice
-		out, err := os.Create((*filename)[:length-4] + ".png")
-		check(err)
-		defer out.Close()
-		png.Encode(out, image)
-	} else if bitdepth == 32 {
+	} else if bitdepth == 12 || bitdepth == 16 || bitdepth == 32 {
 		image := image.NewGray16(image.Rect(0, 0, width, height))
 		image.Pix = fileslice
 		out, err := os.Create((*filename)[:length-4] + ".png")
